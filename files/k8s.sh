759a30adf0b42827951eadde71a7973cc9610d4b
taylor <taylor_liu@kingdee.com>
2019-11-16 10:01:24 +0800

    。。

--- files/k8s.sh
+++ files/k8s.sh
@@ -2,6 +2,6 @@
 # 快速进入k8s容器脚本
 echo "entering docker "$1
 
-CONTAINER_ID=$(./kubectl -s '172.20.176.190:8080' get pods -n ns-retail-dev|grep v7-$1|awk '{print $1}')
+CONTAINER_ID=$(kubectl get pods -n ns-retail-dev|grep v7-$1|awk '{print $1}')
 echo "container id "${CONTAINER_ID}
-./kubectl -s '172.20.176.190:8080' exec -it ${CONTAINER_ID} -n ns-retail-dev /bin/bash
+kubectl exec -it ${CONTAINER_ID} -n ns-retail-dev /bin/bash
